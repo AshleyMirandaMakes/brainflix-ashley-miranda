164599336ac861b47d6a4214f8abbb9dea1a3c19
Ashley <miranda.ash.19@gmail.com>
2021-05-10 21:40:25 -0400

    nav started

--- sprint-one/src/App.js
+++ sprint-one/src/App.js
@@ -1,10 +1,11 @@
 import './App.scss';
+import Nav from './components/Nav'
 import '../src/partials/_resets.scss'
 
 function App() {
   return (
     <div className="App">
-     hello Ashley
+      <Nav />
     </div>
   );
 }
--- sprint-one/src/components/Nav.js
+++ sprint-one/src/components/Nav.js
@@ -1,14 +1,49 @@
-import Logo from '../assets/logos/Logo-brainflix';
+//import React from 'react';
+import Logo from '../assets/logos/Logo-brainflix.svg';
+import UserIcon from '../assets/images/Mohan-muruge.jpg'
+import UploadIcon from '../assets/icons/Icon-upload.svg';
+import SearchIcon from '../assets/icons/Icon-search.svg';
+
+const Button = (props) => {
+    return (
+      <button>{props.icon}{props.name}</button>
+    );
+}
+
+const SearchBar = (props) => {
+  return (
+    <input 
+    type="text" 
+    placeholder={props.icon} 
+    value={props.value}
+    />
+  );
+}
+
+const UserImage = (props) => {
+  return (
+    <div className="nav__image">
+      <img src={props.image} alt="user's image"></img>
+    </div>
+  );
+}
+
+// class Button extends React.Component {
+//   render() {
+//     return (
+//       <button>{this.props.icon}{this.props.name}</button>
+//     );
+//   }
+// }
 
 
 function Nav() {
   return (
     <nav className="nav">
-      Hello world
-      <List />
-      <ListUsingMap />
-      <DisplayGrades />
-      <img src={Logo} alt="A demonstration of Promises" height="150"/>
+      <img src={Logo} alt="Brainflix Logo"/>
+      <SearchBar placeholder={SearchIcon} value="Search"/>
+      <Button icon={UploadIcon} name="UPLOAD"/>
+      <UserImage image={UserIcon} />
     </nav>
   );
 }
