191b44a80667f1de2cc8d7dbbd3f7a0a2502c33f
Ashley <miranda.ash.19@gmail.com>
2021-05-21 13:31:49 -0400

    everything but filtering working

--- sprint-two/src/pages/HomePage/HomePage.js
+++ sprint-two/src/pages/HomePage/HomePage.js
@@ -1,8 +1,8 @@
 import { Component } from 'react';
 import { API_URL, API_KEY, VIDEOS_LIST } from "../../util";
 
-//import Video from '../../components/Video';
-//import FeaturedVideo from '../../components/FeaturedVideo';
+import Video from '../../components/Video';
+import FeaturedVideo from '../../components/FeaturedVideo';
 import VideoList from '../../components/VideoList';
 
 import axios from 'axios';
@@ -11,33 +11,51 @@ class HomePage extends Component {
    state = {
     videos: [],
     videoData : [],
+    defaultVideo: '',
   };
 
   componentDidMount() {
-    const {videoId} = this.props.match.params;
+  const {videoId} = this.props.match.params;
     console.log(videoId);
+  
+  //this sets up the default video'
+  if (this.state.defaultVideo === "" ) {
+     axios.get(`${API_URL}/videos/1af0jruup5gu${API_KEY}`)
+    .then(
+      (response) =>
+      this.setState({
+        videoData: response.data,
+      })
+    )
+    .catch(
+      (error) =>
+      console.log(error)
+    );
+  }
     
-    axios.get(API_URL + VIDEOS_LIST + API_KEY)
+  //this sets up the next video component
+  axios.get(API_URL + VIDEOS_LIST + API_KEY)
     .then ((response) => {
       this.setState({
         videos : [...response.data]
+        
       });    
     }).catch((error) =>
       console.log(error)
-    );
+  );
     
-    axios.get(`${API_URL}/videos/${videoId}${API_KEY}`)
-      .then((response) => {
+  //this pull the video data if a video has been clicked
+  axios.get(`${API_URL}/videos/${videoId}${API_KEY}`)
+    .then((response) => {
         this.setState({
           videoData: [...response.data],
         })
       })
-      .catch((error) => {
+    .catch((error) => {
         console.log(error)
        });
-   };  
+  };  
   
-    
   componentDidUpdate(prevProps) {
     const {videoId} = this.props.match.params;
 
@@ -58,23 +76,25 @@ class HomePage extends Component {
 
   render () {
     console.log("Next Video Array", this.state.videos)
-    console.log("This Video Array", this.state.videoData)
+    console.log("VideoData/CurrentVideo Array", this.state.videoData)
     
-    //this text is not rendering. why?
-    // if (this.state.videoData === null) {
-    //   return <main>Click on a video for more details</main>};
+    // if (this.state.videoData.id === video.id) {
+    //   this.state.videoData.filter((video) => video.id === video.id)};
    
+      // if (this.state.videoData.id === videoId) {
+      //   return <main>Click on a video for more details</main>};
+     
     // {!this.state.videoData? (
     // <p>No Video Selected</p>) : ""
     // } 
 
     return (
       <main>
-      {/* <Video video={this.state.videoData.video} image={this.state.videoData.image}/> */}
-           {/* <div className="app__container"> 
-          <FeaturedVideo videoDetails={this.state.videoData} videoComments={this.state.videoData.comments}/>  */}
+      <Video video={this.state.videoData.video} image={this.state.videoData.image}/>
+           <div className="app__container"> 
+          <FeaturedVideo videoDetails={this.state.videoData} videoComments={this.state.videoData.comments}/> 
               <VideoList videos={this.state.videos}/>
-           {/* </div> */}
+           </div>
       </main>
     )
   }
@@ -82,21 +102,3 @@ class HomePage extends Component {
 
 export default HomePage;
 
-
-   //  erase the two brackets above?
-  //  } else {
-  //   // axios.get(`${API_URL}/videos/1af0jruup5gu${API_KEY}`)
-  //   axios.get("https://project-2-api.herokuapp.com/videos/1af0jruup5gu?api_key=2676221a-58c9-4883-a7b6-19edba276a48")
-  //   .then(
-  //     (response) =>
-  //     this.setState({
-  //       videoData: response.data,
-  //     })
-  //   )
-  //   .catch(
-  //     (error) =>
-  //     console.log(error)
-  //   );
-  //  }
-  // };
-   //how can I add a condition to have the 1st video load if no :videoId has been selected? // also sometimes not iterable which makes sense
