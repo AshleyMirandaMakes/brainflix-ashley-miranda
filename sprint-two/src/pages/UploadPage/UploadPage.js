d3d04bf1b4281fcc3b698a97d4504341afd00b46
Ashley <miranda.ash.19@gmail.com>
2021-05-24 16:19:48 -0400

    styling done

--- sprint-two/src/pages/UploadPage/UploadPage.js
+++ sprint-two/src/pages/UploadPage/UploadPage.js
@@ -45,34 +45,36 @@ class UploadPage extends Component {
       <main>
         <section className="upload">
         <h2 className="upload__heading">Upload Video</h2>
-        <div className="upload__image-container">
-        <h4 className="upload__title">VIDEO THUMBNAIL</h4>
-        <img className="upload__image "src={Thumbnail} alt="upload-image"/>
-        </div>
-        <form className="upload__form" 
-          onSubmit={this.handleFormSubmit}>
-          <div className="upload__form-container">
-          <label className="upload__title">TITLE YOUR VIDEO</label>
-          <input 
-            className="upload__input" 
-            type="text" 
-            name="uploadTitle" 
-            placeholder="Add a title to your video"
-            value={this.state.uploadTitle}
-            onChange={this.handleInputChange} />
-            {this.state.uploadTitle.length >1 && this.state.uploadTitle.length < 10 ? <p>Please enter a title with at least 10 letters.</p> : ''}
-          <label className="upload__title">ADD A VIDEO DESCRIPTION</label>
-          <input 
-            className="upload__textarea" 
-            type="text" 
-            name="uploadDescription" 
-            placeholder="Add a description of your video"
-            value={this.state.uploadDescription}
-            onChange={this.handleInputChange}/>
-            {this.state.uploadDescription.length 
-            > 1 && this.state.uploadDescription.length < 20 ? <p>Please enter a description with at least 20 letters.</p> : ''}
+        <div className="upload__flex-container">
+          <div className="upload__image-container">
+          <h4 className="upload__title">VIDEO THUMBNAIL</h4>
+          <img className="upload__image "src={Thumbnail} alt="upload-image"/>
           </div>
-        </form>
+          <form className="upload__form" 
+            onSubmit={this.handleFormSubmit}>
+            <div className="upload__form-container">
+            <label className="upload__title">TITLE YOUR VIDEO</label>
+            <input 
+              className="upload__input" 
+              type="text" 
+              name="uploadTitle" 
+              placeholder="Add a title to your video"
+              value={this.state.uploadTitle}
+              onChange={this.handleInputChange} />
+              {this.state.uploadTitle.length >1 && this.state.uploadTitle.length < 10 ? <p>Please enter a title with at least 10 letters.</p> : ''}
+            <label className="upload__title">ADD A VIDEO DESCRIPTION</label>
+            <input 
+              className="upload__textarea" 
+              type="text" 
+              name="uploadDescription" 
+              placeholder="Add a description of your video"
+              value={this.state.uploadDescription}
+              onChange={this.handleInputChange}/>
+              {this.state.uploadDescription.length 
+              > 1 && this.state.uploadDescription.length < 20 ? <p>Please enter a description with at least 20 letters.</p> : ''}
+            </div>
+          </form>
+          </div>
         <div className="upload__button-container">
         {!this.state.handleUploadSubmit? <Link to={"/"} className="upload__button" onClick={this.handleUploadSubmit}>PUBLISH</Link> : " "}
           <button className="upload__button--special">CANCEL</button>
